1fb6010e7f989ab1ae88179ef5306f36d5c9f1bd
sinankocatrk <76816180+sinankocatrk@users.noreply.github.com>
2022-08-16 16:46:43 +0300

    v6
    register page is finished

--- user/views.py
+++ user/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import RegisterForm
+from django.contrib.auth.models import User 
+from django.contrib import messages
+from django.contrib.auth import login,authenticate,logout
 
 # Create your views here.
 def register(request):
-    form = RegisterForm()
+
+    form = RegisterForm(request.POST or None)
+    #post olursa RegisterForm(request.POST) olacak
+    
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+
+        newUser = User(username =username)
+        newUser.set_password(password)
+
+        newUser.save()
+        login(request,newUser)
+        messages.info(request,"Başarıyla Kayıt Oldunuz...")
+
+        return redirect("index")
     context = {
-        "form":form
-    }
+            "form" : form
+        }
     return render(request,"register.html",context)
 
 def loginUser(request):
