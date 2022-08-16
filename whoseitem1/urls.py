6fa304cfdd00b22f371825e1095a2b95590e1c01
sinankocatrk <76816180+sinankocatrk@users.noreply.github.com>
2022-08-16 15:10:39 +0300

    v4
    user app

--- whoseitem1/urls.py
+++ whoseitem1/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index,name= "index"),  
     path('about/', views.about,name= "about"), 
-    path('nick/', include("nick.urls")),    
+    path('nick/', include("nick.urls")), 
+    path('user/', include("user.urls")),      
 ]
