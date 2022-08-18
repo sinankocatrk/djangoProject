ec404832aa9470fbabc2f9807b250c5d8635fcb5
sinankocatrk <76816180+sinankocatrk@users.noreply.github.com>
2022-08-18 15:25:07 +0300

    v8
    Crispy and Responsive Web Design

--- whoseitem1/settings.py
+++ whoseitem1/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'nick',
     'user',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
