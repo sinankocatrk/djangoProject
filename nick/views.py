39212101b343182b163e22934381925ea8e447cf
sinankocatrk <76816180+sinankocatrk@users.noreply.github.com>
2022-08-30 16:57:35 +0300

    v20.1
    dynamic url

--- nick/views.py
+++ nick/views.py
@@ -6,7 +6,18 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
 
-    return render(request,"index.html")
+    keyword = request.GET.get("keyword")
+
+    if keyword :
+        articles = Nick.objects.filter(title__contains = keyword)
+        return render(request,"index.html",{"articles":articles})
+
+    articles = Nick.objects.filter()
+    context = {
+        "articles" : articles
+    }
+    return render(request,"index.html",context)
+
 
 def about(request):
     return render(request,"about.html")
